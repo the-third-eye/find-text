caec9d3ad44295e054cd0e4a754b6f51712975b7
Your Name <you@example.com>
2018-05-13 11:58:38 -0500

    added example prog

---
+++ example.cpp
@@ -0,0 +1,25 @@
+#include <tesseract/baseapi.h>
+#include <leptonica/allheaders.h>
+
+int main(){
+
+	char *outText;
+	tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+	// initialize tesseract-ocr with english
+	if(api->Init(NULL, "eng")){
+		fprintf(stderr, "Could not initialize tesseract.\n");
+		exit(1);
+	}	
+	// use leptonica library to open input image
+	Pix *image = pixRead("phototest.tif");
+	api->SetImage(image);
+	// get result from ocr
+	outText = api->GetUTF8Text();
+	printf("OCR output:\n %s", outText);
+	// clean up
+	api->End();
+	delete[] outText;
+	pixDestroy(&image);
+	
+	return 0;
+}
